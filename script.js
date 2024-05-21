28ceb07cf86d8352d072c74bff03fcd37a9714d5
Leonardo Casas <leocabel@hotmail.com>
2024-05-20 21:19:06 -0500

    peticiones

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("peticiones")
+
+const obterPokemon = async () => {
+ const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+const data = await respuesta.json()
+console.log(data)
+}
+obterPokemon()
